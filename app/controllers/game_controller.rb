7bc5569c17ac9181056d4cb9364410486373ed7c
Alonzo Rice <96135977+ricealonzo@users.noreply.github.com>
2024-06-13 23:30:45 +0000

    3

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -14,7 +14,41 @@ class GameController < ApplicationController
       @outcome = "You Lose!"
     end
     
-    render({:template => "/game_views/game"})
+    render({:template => "/game_views/game_rock"})
+  
+  end
+  def rock
+    
+    selection = ["rock","paper","scissors"]
+    
+    @computer_selection = selection.sample
+    
+    if @computer_selection == "paper"
+      @outcome = "Tied"
+    elsif @computer_selection == "rock"
+      @outcome = "You Win!"
+    else  @computer_selection == "scissors"
+      @outcome = "You Lose!"
+    end
+    
+    render({:template => "/game_views/game_paper"})
+  
+  end
+  def scissors
+    
+    selection = ["rock","paper","scissors"]
+    
+    @computer_selection = selection.sample
+    
+    if @computer_selection == "scissors"
+      @outcome = "Tied"
+    elsif @computer_selection == "paper"
+      @outcome = "You Win!"
+    else  @computer_selection == "rock"
+      @outcome = "You Lose!"
+    end
+    
+    render({:template => "/game_views/game_scissors"})
   
   end
 
