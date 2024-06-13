c2a367e02fb1bdd1e381ce288d40c81e298b0692
Alonzo Rice <96135977+ricealonzo@users.noreply.github.com>
2024-06-13 22:13:26 +0000

    1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  
+get("/rock", {:controller => "game_controller", :action => "rock"})
+# get("/paper", :controller=>"game_controller", :action => "paper")
+# get("/scissors", :controller=>"game_controller", :action => "scissors")
+
+
+
+
 
 end
