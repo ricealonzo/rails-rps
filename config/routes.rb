63f9675415370dcdb94ecf3c909b22e505ad3309
Alonzo Rice <96135977+ricealonzo@users.noreply.github.com>
2024-10-24 00:09:22 +0000

    2

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   
 get("/administration", {:controller => "agi_studio", :action => "admin"})   
+
 get("/login", {:controller => "agi_studio", :action => "login"})   
+
 get("/home", {:controller => "agi_studio", :action => "home"})   
+
 get("/appointment", {:controller => "agi_studio", :action => "appointment"})   
 
 end
